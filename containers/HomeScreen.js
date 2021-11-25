c320326741231280bee51e3e0dd7478651e72669
Jean-David EDIGHOFFER <jd.edighoffer@gmail.com>
2021-11-25 13:09:33 +0100

    problème de navigation

--- containers/HomeScreen.js
+++ containers/HomeScreen.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/core";
+import { FontAwesome } from "@expo/vector-icons";
+import axios from "axios";
 import {
   Button,
   Text,
@@ -8,23 +10,24 @@ import {
   Image,
   StyleSheet,
   Dimensions,
+  TouchableOpacity,
+  ActivityIndicator,
 } from "react-native";
-import axios from "axios";
 
-export default function HomeScreen() {
-  const navigation = useNavigation();
+export default function HomeScreen({ navigation }) {
+  // const navigation = useNavigation();
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(
+        const { data } = await axios.get(
           "https://express-airbnb-api.herokuapp.com/rooms"
         );
-        setData(response.data);
+        setData(data);
         setIsLoading(false);
-        console.log(response.data);
+        // console.log(response.data);
       } catch (error) {
         console.log(error.message);
       }
@@ -32,8 +35,33 @@ export default function HomeScreen() {
     fetchData();
   }, []);
 
+  //Gestion des demi-étoiles
+  const displayStars = (ratingValue) => {
+    let tab = [];
+    const isDecimal = !Number.isInteger(ratingValue);
+    const flooredNum = Math.floor(ratingValue);
+
+    for (let i = 1; i <= 5; i++) {
+      if (ratingValue >= i) {
+        tab.push(<FontAwesome name="star" size={24} color="orange" key={i} />);
+      }
+
+      if (ratingValue < i && tab.length < 5) {
+        tab.push(<FontAwesome name="star" size={24} color="grey" key={i} />);
+      }
+
+      if (flooredNum === i && isDecimal) {
+        tab.push(
+          <FontAwesome name="star-half-empty" size={24} color="black" />
+        );
+      }
+    }
+
+    return tab;
+  };
+
   return isLoading ? (
-    <Text>En cours de chargement...</Text>
+    <ActivityIndicator size="large" color="red" style={{ flex: 1 }} />
   ) : (
     <View style={styles.homeContainer}>
       <Image
@@ -47,38 +75,47 @@ export default function HomeScreen() {
         renderItem={({ item }) => {
           return (
             <View>
-              <Image
-                source={{ uri: item.photos[0].url }}
-                style={styles.flatPic}
-                resireMode="contain"
-              />
-              <Text style={styles.price}>{item.price}€</Text>
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate("Room", {
+                    id: item._id,
+                  });
+                }}
+                // style={styles.container}
+              >
+                <Image
+                  source={{ uri: item.photos[0].url }}
+                  style={styles.flatPic}
+                  resireMode="contain"
+                />
+
+                <Text style={styles.price}>{item.price}€</Text>
+              </TouchableOpacity>
               <View style={styles.subPic}>
-                <View>
+                <View style={{ flex: 1 }}>
                   <Text style={styles.title} numberOfLines={1}>
                     {item.title}
                   </Text>
                   <Text>{item.reviews} reviews</Text>
                 </View>
-                <View>
-                  <Image
-                    source={{ uri: item.user.account.photo.url }}
-                    style={styles.hostPic}
-                    resireMode="contain"
-                  />
-                </View>
+
+                <Image
+                  source={{ uri: item.user.account.photo.url }}
+                  style={styles.hostPic}
+                  resireMode="contain"
+                />
               </View>
             </View>
           );
         }}
       />
 
-      <Button
+      {/* <Button
         title="Go to Profile"
         onPress={() => {
           navigation.navigate("Profile", { userId: 123 });
         }}
-      />
+      /> */}
     </View>
   );
 }
@@ -96,36 +133,38 @@ const styles = StyleSheet.create({
     // width: Dimensions.get("window").width * 0.9,
   },
   flatPic: {
-    width: Dimensions.get("window").width,
     height: 200,
-    marginTop: 10,
-    position: "relative",
+    // width: Dimensions.get("window").width * 0.9,
+    // marginTop: 10,
+    // position: "relative",
   },
   hostPic: {
     width: 80,
     height: 80,
     borderRadius: 50,
-    marginRight: 10,
+    marginLeft: 20,
   },
   title: {
     fontSize: 24,
   },
   subPic: {
     flexDirection: "row",
-    // width: Dimensions.get("window").width,
+    width: Dimensions.get("window").width * 0.9,
+    marginBottom: 25,
+    borderBottomColor: "grey",
+    borderBottomWidth: 2,
+    paddingVertical: 8,
     // paddingRight: 60,
     // justifyContent: "space-between",
   },
   price: {
     backgroundColor: "black",
     position: "absolute",
-    bottom: 88,
+    bottom: 10,
     left: 0,
     color: "#fff",
     fontSize: 19,
-    paddingTop: 10,
-    paddingBottom: 10,
-    paddingLeft: 20,
-    paddingRight: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
   },
 });
